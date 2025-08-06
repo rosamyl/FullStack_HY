de8017ee15dd3aaf438f1572bf9ecd12f9778623
Rosa-Maria Myllymäki <rosa.myllymaki@gmail.com>
2025-08-06 18:39:55 +0300

    1C materiaalin mukaiset muutokset

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,43 +1,44 @@
+import { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
   }
   
-
-  const Header = ({ course }) => <h1>{course.name}</h1>
-
-  const Content = ({course}) => (
-    <div>
-      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
-    </div>
-  )
-  const Total = ({course}) => (
-    <p>
-      Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
-    </p>
-  )
-
   return (
     <div>
-      <Header course={course} />
-      <Content course = {course} />
-      <Total course={course} />
+      <Display counter={counter}/>
+
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
