26eba0e9b222cdf35bfcefc8f88e4b07a2d0a13d
rosa-mariamyllym-ki <rosa-maria.ammer@student.oulu.fi>
2026-08-06 17:10:04 +0300

    anekdootit tehtävät 1.12-1.14

---
+++ osa1/anekdootit/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+const Votes = (props) => {
+  return (
+    <div>
+      has {props.votes} votes
+    </div>
+  )
+}
+
+const MostVoted = (props) => {
+  const maxVotedIndex = props.votes.indexOf(Math.max(...props.votes))
+  console.log("Max voted index:", maxVotedIndex, "Votes:", props.votes[maxVotedIndex])
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[maxVotedIndex]}
+      <Votes votes={props.votes[maxVotedIndex]}/>
+    </div>
+  )
+
+}
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const handleClick = () => {
+    while (true) {
+      const randomIndex = Math.floor(Math.random() * anecdotes.length)
+      if (randomIndex !== selected) {
+        setSelected(randomIndex)
+        break
+      }
+    }
+  }
+
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    console.log("Votes:", copy)
+
+  }
+  
+
+  return (
+    <>
+      <div>
+        <h1>Anecdote of the day</h1>
+        {anecdotes[selected]}
+      </div>
+      <div>
+        <Votes votes={votes[selected]}/>
+      </div>
+      <div>
+        <Button handleClick={handleClick} text={"next anecdote"}/>
+        <Button handleClick={handleVote} text={"vote"}/>
+        <MostVoted votes={votes} anecdotes={anecdotes}/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
