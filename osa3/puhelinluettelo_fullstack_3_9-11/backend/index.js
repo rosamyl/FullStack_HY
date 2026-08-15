1fefdceba2033bc6a6c6c0acbaf044bc97255a2b
rosa-mariamyllym-ki <rosa-maria.ammer@student.oulu.fi>
2026-08-15 16:04:31 +0300

    teht. 3.11

--- osa3/puhelinluettelo_fullstack_3_9-11/backend/index.js
+++ osa3/puhelinluettelo_fullstack_3_9-11/backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(express.static(dist))
+
 let persons = [
     { 
     "name": "Arto Hellas", 
