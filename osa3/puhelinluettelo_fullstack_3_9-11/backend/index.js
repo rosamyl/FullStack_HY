189105610772dd07f5261db7a197db8fb0077fde
rosa-mariamyllym-ki <rosa-maria.ammer@student.oulu.fi>
2026-08-15 15:16:33 +0300

    debug morgan

--- osa3/puhelinluettelo_fullstack_3_9-11/backend/index.js
+++ osa3/puhelinluettelo_fullstack_3_9-11/backend/index.js
@@ -3,9 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 app.use(cors())
 let persons = [
     { 
@@ -98,6 +96,7 @@ app.post('/api/persons', (request, response) => {
     return JSON.stringify(request.body)})
 })
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 const PORT = 3001
 app.listen(PORT)
